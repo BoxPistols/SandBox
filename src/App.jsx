77af11857fe22d22dcc4e406fa28c7c9fcfae977
A_ito <BoxPistols@users.noreply.github.com>
2021-01-30 21:29:32 +0900

    Components of Functional / Class

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "./stories/components/Button";
+import logo from './logo.svg'
+import './App.css'
+import Button from './stories/components/Button'
+import Header from './Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="primary">import by Story</Button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />π{' '}
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Button variant="primary">import by Story</Button>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+// functional Component
+const Header = () => {
+    return (
+        <header>
+            <h1>Functional Class Component</h1>
+        </header>
+    )
+}
+// class
+// class Header extends React.Component {
+//     render() {
+//         return <h1>Class Component</h1>
+//     }
+// }
+
+export default Header
