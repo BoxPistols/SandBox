4993f5df0bc5f77082737f8b2c6a04dabba098cf
A_ito <BoxPistols@users.noreply.github.com>
2021-01-30 22:21:12 +0900

    Add PropTypes import PropTypes from 'prop-types'
    Header.propTypes = {
      title: PropTypes.string.isRequired,
    }

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './App.css'
 
 // functional Component
-const Header = (props) => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     </header>
   )
 }
 
-// class
-// class Header extends React.Component {
-//     render() {
-//         return <h1>Class Component</h1>
-//     }
-// }
-
 Header.defaultProps = {
-  title: 'The React',
+  title: 'The React!',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default Header
