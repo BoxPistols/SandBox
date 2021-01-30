e3c1cf1c33454a7981780b9100e538df0ee22670
A_ito <BoxPistols@users.noreply.github.com>
2021-01-30 21:42:00 +0900

    [Props] Default Props set Props Default

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Button from './stories/components/Button'
 import Header from './Header'
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />π{' '}
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <Button variant="primary">import by Story</Button>
-            </header>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Header />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>π</code>
+        </p>
+        <Button variant="primary">import by Story</Button>
+      </header>
+    </div>
+  )
 }
 
 export default App
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import './App.css'
 
 // functional Component
-const Header = () => {
-    return (
-        <header>
-            <h1>Functional Class Component</h1>
-        </header>
-    )
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  )
 }
+
 // class
 // class Header extends React.Component {
 //     render() {
 //         return <h1>Class Component</h1>
 //     }
 // }
+
+Header.defaultProps = {
+  title: 'The React',
+}
 
 export default Header
